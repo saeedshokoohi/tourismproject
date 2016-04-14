35a642c7e7b1b800a46e9f4c5754467fe4e5f5d8
saeed <123>
2016-04-14 12:05:59 +0430

    adding dto

---
+++ dto/src/main/java/base/InfoReferenceDto.java
@@ -0,0 +1,7 @@
+package base;
+
+/**
+ * Created by saeed on 4/14/2016.
+ */
+public class InfoReferenceDto {
+}
--- dto/src/main/java/vote/VoteValuesDto.java
+++ dto/src/main/java/vote/VoteValuesDto.java
@@ -4,4 +4,50 @@ package vote;
  * Created by saeed on 4/14/2016.
  */
 public class VoteValuesDto {
+
+    double OneStarCount;
+    double TwoStarCount;
+    double ThreeStarCount;
+    double FourStarCount;
+    double FiveStarCount;
+
+    public double getOneStarCount() {
+        return OneStarCount;
+    }
+
+    public void setOneStarCount(double oneStarCount) {
+        OneStarCount = oneStarCount;
+    }
+
+    public double getTwoStarCount() {
+        return TwoStarCount;
+    }
+
+    public void setTwoStarCount(double twoStarCount) {
+        TwoStarCount = twoStarCount;
+    }
+
+    public double getThreeStarCount() {
+        return ThreeStarCount;
+    }
+
+    public void setThreeStarCount(double threeStarCount) {
+        ThreeStarCount = threeStarCount;
+    }
+
+    public double getFourStarCount() {
+        return FourStarCount;
+    }
+
+    public void setFourStarCount(double fourStarCount) {
+        FourStarCount = fourStarCount;
+    }
+
+    public double getFiveStarCount() {
+        return FiveStarCount;
+    }
+
+    public void setFiveStarCount(double fiveStarCount) {
+        FiveStarCount = fiveStarCount;
+    }
 }
